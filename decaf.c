d016914a92512717a71e3d2477953195b3d07f1c
Siham JANATI <sihamjanati97@hotmail.com>
2021-12-21 23:19:02 +0100

    options d'executions [#45]

--- decaf.c
+++ decaf.c
@@ -7,21 +7,19 @@ extern int yyparse();
 extern int yylex();
 extern int yydebug;
 extern FILE* fout;
-extern size_t tmpCount, nextquad;
 
 int main(int argc, char* argv[]){
     
-
     //yydebug=1;
     curr_context = NULL;
     int t = yyparse();
-    //fclose(source);
     if (t==0)
         add_labels();    
     
     int i = 1;
 
     while(i<argc){
+
         if(!strcmp(argv[i],"-version")){
             printf("\nXU Chenglin  -  JANATI Siham  -  HAMMER Tom  -  WEREY Laurent\n");
             printf("SIL          .  SDSC          .  SIL         .  SIL\n\n");
@@ -29,21 +27,26 @@ int main(int argc, char* argv[]){
         else if(!strcmp(argv[i],"-tos")){
             print_stack();
         }
-        else if(!strcmp(argv[i],"-i")){
-            print_globalcode();   
-        }
         else if(!strcmp(argv[i],"-o")){
             if(i+1>=argc){
-                fprintf(stderr, "\nUsage: %s source [-version] [-tos] [-i] [-o <name>]\n\n",argv[0]);
-                exit(0);
+                fout = fopen("out.asm","w");
             }
-            fout = fopen(argv[i+1],"w");
-            if(fout == NULL){
-                printf("\nImpossible de créer le fichier %s\n",argv[i+1]);
+            else if(!strcmp(argv[i+1],"stdout")){
                 fout = stdout;
+                i++;
+            }
+            else if((fout = fopen(argv[i+1],"w"))==NULL){
+                fout = fopen("out.asm","w");
+                i++;
             }
             translate();
-            fclose(fout);
+            if(fout != stdout)
+                fclose(fout);
+            
+        }
+        else{
+            fprintf(stderr, "\nUsage: %s [-version] [-tos] [-i] [-o [<name>]]  <  source \n\n",argv[0]);
+            exit(0);
         }
         i++;
     }
